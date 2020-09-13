84717211aa156c3def4bcce675b149c748603707
samuel <amit.samuel@web.de>
2020-09-13 23:03:02 +0200

    Install  basic HTTP authentication and JWT authentication with Passport new file, “passport.js” define  “LocalStrategy” and “JWTStrategy.” new file, “auth.js

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
-const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const Models = require('./models.js');
-
 const Movies = Models.Movie;
 const Users = Models.User;
+const passport = require('passport');
+require('./passport');
+
+const uuid = require("uuid");
+
 mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
-
-const express = require("express"),
-    bodyParser = require("body-parser"),
-    uuid = require("uuid");
-const morgan = require("morgan");
-const app = express();
-
-app.use(bodyParser.json());
 app.use(morgan("common"));
+app.use(bodyParser.json());
+
+let auth = require('./auth')(app);
+
 
 // Get list of movies
 // Get all users
-app.get("/movies", (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
     Movies.find()
         .then((movies) => {
             res.status(201).json(movies);
         })
-        .catch((err) => {
-            res.status(500).send("Error: " + err);
+        .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
         });
 });
 
 //Get data about a single movie by name
-app.get("/movies/:Title", (req, res) => {
+app.get("/movies/:Title", passport.authenticate('jwt', { session: false }),(req, res) => {
         Movies.findOne({ Title: req.params.Title })
             .then((movie) => {
                 res.json(movie);
@@ -39,7 +44,7 @@ app.get("/movies/:Title", (req, res) => {
 );
 
 // Get the data about Genre name and description by movie title
-app.get("/movies/Genre/:Title", (req, res) => {
+app.get("/movies/Genre/:Title", passport.authenticate('jwt', { session: false }),(req, res) => {
         Movies.findOne({ Title: req.params.Title })
             .then((movie) => {
                 res
@@ -59,7 +64,7 @@ app.get("/movies/Genre/:Title", (req, res) => {
 
 //Return data about a director
 
-app.get("/movies/Director/:Name", (req, res) => {
+app.get("/movies/Director/:Name", passport.authenticate('jwt', { session: false }),(req, res) => {
         Movies.findOne({ "Director.Name": req.params.Name })
             .then((movies) => {
                 res.json(
@@ -78,13 +83,12 @@ app.get("/movies/Director/:Name", (req, res) => {
 
 //User
 //Allow new users to register
-
 app.post("/users", (req, res) => {
         //check the validation object for errors
-     Users.findOne({ Username: req.body.userName })
+        Users.findOne({ Username: req.body.userName })
             .then((user) => {
                 if (user) {
-                    return res.status(400).send(req.body.Username + "already exists");
+                    return res.status(400).send(req.body.userName + "already exists");
                 } else {
                     Users.create({
                         userName: req.body.userName,
@@ -107,7 +111,7 @@ app.post("/users", (req, res) => {
 );
 
 //Get data about a single user by name
-app.get('/users/:name', (req, res) => {
+app.get('/users/:name',(req, res) => {
     res.json(users.find((user) => {
         return user.name === req.params.name;
     }));
@@ -115,7 +119,7 @@ app.get('/users/:name', (req, res) => {
 
 
 //Allow existing users to deregister
-app.delete('/users/:name', (req, res) => {
+app.delete('/users/:name',passport.authenticate('jwt', { session: false }),(req, res) => {
         Users.findOneAndRemove({ userName: req.params.name })
             .then((user) => {
                 if (!user) {
