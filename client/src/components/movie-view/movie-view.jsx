4b56fc382a95991c57c91bd01394a3417adaa2a3
samuel <amit.samuel@web.de>
2020-11-07 20:55:31 +0100

    remove button to fix error

--- client/src/components/movie-view/movie-view.jsx
+++ client/src/components/movie-view/movie-view.jsx
@@ -40,7 +40,7 @@ export class MovieView extends React.Component {
                     <span className="label">Director: </span>
                     <span className="value">{movie.Director.Name}</span>
                 </div>
-                <button onClick={returnToMain}>Back</button>
+
 
             </div>
 
